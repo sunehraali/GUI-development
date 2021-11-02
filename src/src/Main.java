bf6ef2f5e00ae7a1d93d2ebfa597a3a34e0c0ac0
sunehraali <sunehra.ali@mail.utoronto.ca>
2021-11-02 09:34:23 -0400

    new

---
+++ src/src/Main.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Stackpane;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+    public static void main(String[] args){
+        system.out
+    }
+}
